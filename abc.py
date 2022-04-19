dc3f328d2d84d91ea2cf50af5ba48c2fd7bcb950
B. Si <46674147+dev7060@users.noreply.github.com>
2022-04-19 17:05:23 +0530

    Update abc.py

--- abc.py
+++ abc.py
@@ -10,3 +10,22 @@ while(i<=num):
         temp = int(temp / 10)
     i = i+1
 print(counter)
+
+############################################
+
+num = int(input())
+i=2
+while(i<=num):
+    temp = i
+    j = 2
+    flag = 1
+    while(j<=(temp//2)):
+        if(temp%j == 0):
+            flag=0
+            break
+        j = j+1
+    if(flag==1):
+        print(temp, end=" ")
+    i = i+1
+    
+ ############################################
